fffb4d2cdb1208ab6fa66e1e3a2df31036d367ca
Prakash Muthusamy <prakash.muthusamy@cesltd.com>
2021-10-06 16:22:30 +0530

    ass-17 added algorithms

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # JavaScript Object Path traversal algorithm
+
+### Algorithm for find path JSON object
+
+* step 1: write function definition and get object and path string
+ * step 2: split the path string and store into the new array as arr
+ * step 3: assign object into new variable as res
+ * step 4: use for loop and iterate the arr elements
+ * step 5: check if res of arr element is defined or not
+ * step 6: if not print undefined
+ * step 7: if yes stored that object element into the res
+ * step 8: finish the iteration print the res
--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -16,6 +16,20 @@ var obj = {
     }
 };
 
+
+/**
+ * Algorithm
+ * step 1: write function definition and get object and path string
+ * step 2: split the path string and store into the new array as arr
+ * step 3: assign object into new variable as res
+ * step 4: use for loop and iterate the arr elements
+ * step 5: check if res of arr element is defined or not
+ * step 6: if not print undefined
+ * step 7: if yes stored that object element into the res
+ * step 8: finish the iteration print the res
+ */
+
+
 function findPath(obj, str) {
     const arr = str.split('.');
     var res = obj;
